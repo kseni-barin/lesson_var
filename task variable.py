d6cbd56f6c8b798b6789e4d6e5e737c9d533e4f5
kseni-barin <kmalyutkina@yandex.ru>
2024-08-27 08:57:10 +0300

    Add files via upload

---
+++ task variable.py
@@ -0,0 +1,7 @@
+homework_task_count = 12
+spent_hours_count = 1.5
+name_of_course = 'Python'
+time_for_one_task = spent_hours_count / homework_task_count
+
+print ('Курс: ', name_of_course, ', всего задач: ' , homework_task_count, ', затрачено часов: ', spent_hours_count,', среднее время выполнения ',  time_for_one_task, ' часа.', sep='')
+
